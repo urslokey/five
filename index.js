3793cf52a29e08fee0325cdfd8999842fb5993af
unknown <lokey1410@gmail.com>
2020-12-02 22:58:34 +0530

    first

---
+++ index.js
@@ -0,0 +1,21 @@
+const close =document.querySelector('.close-btn');
+const modal =document.querySelector('.modal-btn');
+const overlay =document.querySelector('.modal-overlay');
+
+modal.addEventListener('click',showModal);
+close.addEventListener('click',closeModal);
+
+function showModal(){
+
+        overlay.classList.add('open-modal');
+
+
+}
+
+function closeModal(){
+
+    overlay.classList.remove('open-modal');
+}
+
+setInterval(closeModal,2000);
+
